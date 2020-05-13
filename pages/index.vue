7335acb6dbf1c13e732a2fe5872e92601eab5982
Grifon1525 <65287278+Grifon1525@users.noreply.github.com>
2020-05-13 19:51:51 +0700

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -5,5 +5,11 @@
 </template>
 
 <script>
-export default {};
+export default {
+  head() {
+    return {
+      script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
+    };
+  },
+};
 </script>
